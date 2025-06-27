c4ba0f8099a70cdd7db081b5a0991ddabd9c25fd
Muskan-me <mukum517@gmail.com>
2025-06-27 22:28:43 +0530

    something is added

--- string.js
+++ string.js
@@ -1,4 +1,14 @@
-let items=[];
+//Task 1........................
+
+
+/*let fruits=["apple", "mango", "orange"]
+let fruits2=["banana"]
+let add=fruits.concat(fruits2);
+console.log(add);*/
+
+//Task 2..................
+
+/*let items=[];
 function fruits(name)
 {
     let newItem=items.find(i=>i.Name===name);
@@ -8,5 +18,19 @@ function fruits(name)
 fruits("apple");
 fruits("mango");
 fruits("orange");
+
+console.log(items);*/
+
+//Task 3..................
+let items=[];
+function fruits(name,count)
+{
+    let newItem=items.find(i=>i.Name===name);
+    newItem?newItem.count+=count:items.push({name,count});
+    
+}
+fruits("apple",5);
+fruits("mango",7);
+fruits("orange",3);
 
 console.log(items);
