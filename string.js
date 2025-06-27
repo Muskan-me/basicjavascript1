92f1f4b898a26b8bc665f21905d655e8682c6304
Muskan-me <mukum517@gmail.com>
2025-06-27 22:16:24 +0530

    something is added

--- string.js
+++ string.js
@@ -1,1 +1,12 @@
-console.log("hello")
+let items=[];
+function fruits(name)
+{
+    let newItem=items.find(i=>i.Name===name);
+    newItem?newItem.count++ : items.push({name,count:1});
+    
+}
+fruits("apple");
+fruits("mango");
+fruits("orange");
+
+console.log(items);
